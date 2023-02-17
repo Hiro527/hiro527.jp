7b037ecb17584181bc5e754cf45dd0a1703c79ba
Hiro527 <hiro527.dev@gmail.com>
2023-02-17 21:15:41 +0900

    [ADD] 疲れちゃったからここまでw

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,25 @@
+/**
+ * GA4の有効化/無効化を行う
+ * @param {Boolean} value
+ */
+const setGA4 = async (value) => {
+    window['ga-disable-G-0HTVBVVGX2'] = value;
+    return;
+};
+
+/**
+ * Cookieの設定を表示する
+ */
+const showCookieConfig = async () => {
+    // wip
+    return;
+};
+
 window.onload = () => {
-    console.log('OK');
+    // 初アクセス時の処理
+    const firstAccess = localStorage.getItem('firstAccess');
+    if (!firstAccess) {
+        localStorage.setItem('firstAccess', new Date());
+        showCookieConfig();
+    }
 };
