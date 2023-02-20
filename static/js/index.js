71884369ac160147d8942289e860637122e9df27
Hiro527 <hiro527.dev@gmail.com>
2023-02-21 01:06:00 +0900

    [ADD] 表示のアニメーションを追加

--- static/js/index.js
+++ static/js/index.js
@@ -8,10 +8,14 @@ const showCookieConfig = async () => {
     return;
 };
 
-const copyEmailAddr = () => {
+const copyEmailAddr = (el) => {
     navigator.clipboard
         .writeText('business@hiro527.jp')
         .catch((e) => console.error(e));
+    el.innerText = 'Copied!';
+    setTimeout(() => {
+        el.innerText = 'Click to Copy';
+    }, 3000);
 };
 
 const openNewUrl = (url) => {
@@ -21,8 +25,9 @@ const openNewUrl = (url) => {
 
 window.onload = () => {
     // 初アクセス時の処理
+    const searchParams = new URLSearchParams(window.location.search);
     const firstAccess = localStorage.getItem('firstAccess');
-    if (!firstAccess) {
+    if (!firstAccess || searchParams.has('debug')) {
         localStorage.setItem('firstAccess', new Date());
         showCookieConfig();
     }
